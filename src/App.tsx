e4c16b8ec3da79d5c6024c6bfcfbd7dcbdd44494
draysongz <dharmiegra4@gmail.com>
2024-07-01 14:56:17 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Referral from "./pages/Referral"
 import { useUserData } from "./hooks/useUserData"
-import Spinner from "./components/Spinner"
+// import Spinner from "./components/Spinner"
 import { ContextProvdider } from "./context/ContextProvider"
 import WebApp from "@twa-dev/sdk"
 import { useEffect, useState } from "react"
@@ -10,27 +10,31 @@ import Boost from "./components/Boost"
 import Level from "./components/Level"
 import Stats from "./pages/Stats"
 function App() {
-  const [userId, setUserId] = useState<number>()
-  const [firstName, setFirstName] = useState<string | null>(null)
+  // const [userId, setUserId] = useState<number>()
+  // const [firstName, setFirstName] = useState<string | null>(null)
   const params = new URLSearchParams(location.search)
   // const userId = Number(params.get("userId"))
   const referralId = Number(params.get("referralId"))
+  const userId = 1
+  const firstName = 'habibilord'
   // const firstName = params.get("name")
+
+ 
 
   const { isLoading, name } = useUserData(userId, firstName, referralId)
 
-  useEffect(() => {
-    WebApp.expand()
-    const id = WebApp.initDataUnsafe.user?.id
-    const name = WebApp.initDataUnsafe.user?.first_name || null
-    if (!id && !name) return
-    setUserId(id)
-    setFirstName(name)
-  }, [])
+  // useEffect(() => {
+  //   WebApp.expand()
+  //   const id = WebApp.initDataUnsafe.user?.id
+  //   const name = WebApp.initDataUnsafe.user?.first_name || null
+  //   if (!id && !name) return
+  //   setUserId(id)
+  //   setFirstName(name)
+  // }, [])
 
-  if (isLoading) {
-    return <Spinner />
-  }
+  // if (isLoading) {
+  //   return <Spinner />
+  // }
   return (
     <ContextProvdider
       userId={userId}
--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,26 +1,26 @@
-import { Flex, Box, Text } from "@chakra-ui/react"
-import { ClipLoader } from "react-spinners"
+// import { Flex, Box, Text } from "@chakra-ui/react"
+// import { ClipLoader } from "react-spinners"
 
-function Spinner() {
-  return (
-    <Flex
-      bg={"url(/splash.jpg)"}
-      bgRepeat={"no-repeat"}
-      bgSize={"cover"}
-      bgPos={"center"}
-      h={"100%"}
-      overflow={"hidden"}
-      justify={"center"}
-      align={"center"}
-    >
-      <Box textAlign={"center"}>
-        <ClipLoader color="#3182CE" size={70} />
-        <Text color={"white"} fontSize={"25px"} fontWeight={"bold"}>
-          Loading...
-        </Text>
-      </Box>
-    </Flex>
-  )
-}
+// function Spinner() {
+//   return (
+//     <Flex
+//       bg={"url(/splash.jpg)"}
+//       bgRepeat={"no-repeat"}
+//       bgSize={"cover"}
+//       bgPos={"center"}
+//       h={"100%"}
+//       overflow={"hidden"}
+//       justify={"center"}
+//       align={"center"}
+//     >
+//       <Box textAlign={"center"}>
+//         <ClipLoader color="#3182CE" size={70} />
+//         <Text color={"white"} fontSize={"25px"} fontWeight={"bold"}>
+//           Loading...
+//         </Text>
+//       </Box>
+//     </Flex>
+//   )
+// }
 
-export default Spinner
+// export default Spinner
